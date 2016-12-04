d0292ba0b4275e7c2ce7d1ededee8815f5fc634a
潘兴龙 <sysupanxl6@gmail.com>
2016-12-04 23:24:54 +0800

    添加了全局常量说明

---
+++ constants.py
@@ -0,0 +1,9 @@
+# coding=utf-8
+# author=XingLong Pan
+# date=2016-12-04
+
+
+DOUBAN_MOVIE_LOGIN_URL = 'https://accounts.douban.com/login'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) ' \
+             'AppleWebKit/537.36 (KHTML, like Gecko) ' \
+             'Chrome/54.0.2840.71 Safari/537.36'
