4427644c7ac814042a8270925a8af9591886b01d
潘兴龙 <sysupanxl6@gmail.com>
2016-12-08 23:10:41 +0800

    添加了电影元数据的定义

---
+++ parser/Entity.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# coding=utf-8
+# author=XingLong Pan
+# date=2016-12-05
+
+movie = {
+    'douban_id': 0,
+    'title': '',
+    'directors': '',
+    'scriptwriters': '',
+    'actors': '',
+    'types': '',
+    'release_region': '',
+    'release_date': '',
+    'alias': '',
+    'languages': '',
+    'duration': 0,
+    'score': 0.0,
+    'description': '',
+    'tags': '',
+    'link': '',
+    'posters': ''
+}
