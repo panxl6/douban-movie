61d49cccb60d83c1a4aa61aed97401f3477d3b22
潘兴龙 <1209384400@qq.com>
2017-01-15 23:11:16 +0800

    更新了配置文件

--- doc/movie.sql
+++ doc/movie.sql
@@ -1,4 +1,3 @@
-
 drop database if exists douban;
 
 create database douban default charset utf8;
@@ -7,22 +6,38 @@ use douban;
 
 drop table if exists movie;
 create table movie (
-`id` int not null auto_increment,
-`douban_id` int,
+`douban_id` varchar(16) not null,
 `title` varchar(1024),
-`directors` varchar(128),
-`scriptwriters` varchar(128),
-`actors` varchar(1024),
-`types` varchar(256),
-`release_region` varchar(128),
-`release_date` varchar(128),
-`alias` varchar(126),
-`languages` varchar(128),
-`duration` int,
-`score` float,
+`directors` text,
+`scriptwriters` text,
+`actors` text,
+`types` text,
+`release_region` text,
+`release_date` text,
+`alias` text,
+`languages` text,
+`duration` text,
+`score` text,
 `description` text,
-`tags` varchar(1024),
-`link` varchar(32),
-`posters` varchar(1024),
+`tags` text,
+primary key(`douban_id`)
+) charset=utf8;
+
+/* type说明：
+1表示剧照，
+2表示海报，
+3表示壁纸
+
+完整的图片url为:
+https://movie.douban.com/photos/photo/photo_id
+example:
+https://movie.douban.com/photos/photo/2285200316/
+*/
+drop table if exists photo;
+create table photo (
+`id` int not null auto_increment,
+`douban_id` varchar(16),
+`type` tinyint,
+`photo_id` varchar(16),
 primary key(`id`)
 ) charset=utf8;
