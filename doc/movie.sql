17ad1e8a93149ddff5ed4f06e09d6e0c5e9c27e2
ubuntu-home <sysupanxl6@gmail.com>
2019-12-29 12:00:48 +0800

    【doc】完善数据库设计说明

--- doc/movie.sql
+++ doc/movie.sql
@@ -1,27 +1,27 @@
 drop database if exists douban;
 
-create database douban default charset utf8;
+create database douban default charset utf8mb4;
 
 use douban;
 
 drop table if exists movie;
 create table movie (
-`douban_id` varchar(16) not null,
-`title` varchar(1024),
-`directors` text,
-`scriptwriters` text,
-`actors` text,
-`types` text,
-`release_region` text,
-`release_date` text,
-`alias` text,
-`languages` text,
-`duration` text,
-`score` text,
-`description` text,
-`tags` text,
-primary key(`douban_id`)
-) charset=utf8;
+    `douban_id` varchar(16) not null primary key comment '豆瓣的标记id当主键,顺便用来去重',
+    `title` varchar(1024) not null default '' comment '标题',
+    `directors` text comment  '导演',
+    `scriptwriters` text comment '编剧',
+    `actors` text comment '演员',
+    `types` text comment '类别',
+    `release_region` text comment '上映地区',
+    `release_date` text comment '上映日期',
+    `alias` text comment '别名',
+    `languages` text comment '语言',
+    `duration` text comment '播放时长',
+    `score` text comment '评分',
+    `description` text comment '描述',
+    `tags` text comment '标签',
+    `create_at` timestamp not null default current_timestamp
+) engine=innodb default charset=utf8mb4;
 
 /* type说明：
 1表示剧照，
